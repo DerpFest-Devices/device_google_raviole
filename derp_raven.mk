8fc37b377350534c1b508130a8f1ef9de51eb063
Einar Gednochsson <pete80pro.pb@gmail.com>
2023-07-11 16:44:38 +0000

    raviole: Derpfest Official

--- derp_raven.mk
+++ derp_raven.mk
@@ -6,18 +6,25 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
 $(call inherit-product, device/google/gs101/lineage_common.mk)
 
 include device/google/raviole/raven/device-lineage.mk
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+DERP_BUILDTYPE := Official
+EXTRA_UDFPS_ANIMATIONS := true
+USE_LEGACY_BOOTANIMATION := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6 Pro
-PRODUCT_NAME := lineage_raven
+PRODUCT_NAME := derp_raven
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
