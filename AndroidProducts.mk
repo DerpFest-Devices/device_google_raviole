197483d2e486abe6913e8c173cf7b1b9b219aea6
Einar Gednochsson <pete80pro.pb@gmail.com>
2024-05-05 12:21:05 +0200

    raviole: derp it

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -37,8 +37,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_whitefin_hwasan.mk \
     $(LOCAL_DIR)/aosp_whitefin_64.mk \
     $(LOCAL_DIR)/factory_whitefin.mk \
-    $(LOCAL_DIR)/lineage_oriole.mk \
-    $(LOCAL_DIR)/lineage_raven.mk
+    $(LOCAL_DIR)/derp_oriole.mk \
+    $(LOCAL_DIR)/derp_raven.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-trunk_staging-userdebug \
